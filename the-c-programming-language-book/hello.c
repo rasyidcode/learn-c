79c0737f4acc4461e7d7659f2a923fc1171623c2
rasyidcode <rasyidcode7@gmail.com>
2025-12-10 02:59:00 +0700

    initial commit

---
+++ the-c-programming-language-book/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // include information about standard library
+
+int main() // define a function called main that received no argument values
+{          // statements of main are enclosed in braces
+  printf("hello, world!\n"); // main calls library functions printf to print
+                             // this sequence of characters
+                             // \n represents the newline character
+}
