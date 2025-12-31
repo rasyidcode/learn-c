b0f2146ef7636bf766d4a179f0a837a2e85bda86
Ahmad Rasyid <ahmad.rasyid@arbitersports.com>
2025-12-31 17:01:07 +0700

    update

---
+++ args_practice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **args) {
+    int i;
+
+    printf("Total number of args: %d\n", argc);
+
+    int length = sizeof(args) / sizeof(args[0]);
+    for (i = 0; i < argc; i++) {
+        printf("Arg #%d => %s\n", i+1, args[i]);
+    }
+
+    return 0;
+}
