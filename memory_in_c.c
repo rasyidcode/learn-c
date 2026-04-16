1d4e23ab1c0ffb8981c39d405dfddfe4fb12b5a2
rasyidcode <rasyidcode7@gmail.com>
2026-04-16 23:16:07 +0700

    update

---
+++ memory_in_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    float temp;
+    double pi;
+    char initial;
+
+    printf("%zu\n", sizeof(year));
+    printf("%zu\n", sizeof(temp));
+    printf("%zu\n", sizeof(pi));
+    printf("%zu\n", sizeof(initial));
+
+    return 0;
+}
