8855dcf246e9ce8928957f4707f4c72802569793
Jamil Al Rasyid <rasyidcode7@gmail.com>
2026-06-13 15:30:48 +0700

    update

--- math-stuff/math_09_arch_length.c
+++ math-stuff/math_09_arch_length.c
@@ -5,14 +5,37 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-float deg2rad(int deg) {
+float deg2rad(float deg) {
     return deg * M_PI / 180.0;
 }
 
 int main(void) {
-    int radius;
-    int degree;
+    float radius;
+    float degrees;
+    float radians;
+    float arcLength;
 
+    printf("Enter circle radius: ");
+    scanf("%f", &radius);
+
+    printf("Enter angle in degrees: ");
+    scanf("%f", &degrees);
+
+    // convert degrees to radians
+    radians = deg2rad(degrees);
+
+    // arc length
+    // formula: s = radius * radians
+    arcLength = radius * radians;
+
+    printf("\n--- Result ---\n");
+    printf("Angle in degrees : %.2f degrees\n", degrees);
+    printf("Angle in radians : %.4f radians\n", radians);
+    printf("Arc Length       : %.4f\n", arcLength);
+
+    printf("\nUsing radians in math functions:\n");
+    printf("sin(%.4f) = %.4f\n", radians, sinf(radians));
+    printf("cos(%.4f) = %.4f\n", radians, cosf(radians));
 
     return 0;
 }
